5ccda2b91f71670fb58a8c60a6001636f52ece4b
Gary Woodfine <lnb0l9dc@duck.com>
2024-03-30 20:39:47 +0000

    Add exception handling for invalid database connections
    This update introduces a new unit test to verify the behaviour when an application attempts to connect to a database with an invalid connection string. This will ensure that the application handles failed database connections appropriately and throws an exception to alert the issue.

--- tests/Unit/DependencyInjectionTests.cs
+++ tests/Unit/DependencyInjectionTests.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Nostrfi.Relay.Persistence;
@@ -36,4 +37,22 @@ public class DependencyInjectionTests
         // Act
         Should.Throw<NostrDbException>(() => services.AddNostrDatabase(configuration));
     }
+    
+    [Fact]
+    [Description("The connection string is a valid format but cannot validate because cannot conenct")]
+    public void AddNostrDatabase_Throws_Exception_No_Validate_ConnectionString()
+    {
+        var services = new ServiceCollection();
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string>
+            {
+                { "ConnectionStrings:nostrfi", "User ID=root;Password=myPassword;Host=localhost;Port=5432;Database=myDataBase;Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Lifetime=0;" }
+            })
+            .Build();
+
+        // Act
+        Should.Throw<NostrDbException>(() => services.AddNostrDatabase(configuration));
+    }
+    
+    
 }
