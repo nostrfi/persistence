631a774b1a367a1e57661db151376041aa22b54f
Gary Woodfine <lnb0l9dc@duck.com>
2024-12-02 21:12:01 +0000

    Fix multiline comment formatting in NostrContextFactory.cs
    Corrected the formatting of a multline comment by removing an extra set of slashes. This change clarifies the purpose of the factory class used by EF Core tools and maintains the exclusion from code coverage, emphasizing that it's only meant for migration generation.

--- src/Persistence/NostrContextFactory.cs
+++ src/Persistence/NostrContextFactory.cs
@@ -5,9 +5,8 @@ namespace Nostrfi.Persistence;
 
 [ExcludeFromCodeCoverage]
 /// <summary>
-///     ///     This is a factory class that is used by the EF Core tools to create an instance of the application db
-///     context
-///     only for the purpose of generating the migrations.  We never use it to create an instance of the db context
+///     This is a factory class that is used by the EF Core tools to create an instance of the application db
+///     context only for the purpose of generating the migrations.  We never use it to create an instance of the db context
 ///     for the application to use.  The connection string it purposefully set to a dummy value that will never be used
 ///     and it is not meant to be pointed to any real database or Local DB instance.
 ///     Intentionally excluded from Code Coverage because this is just for development use
